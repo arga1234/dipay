9a6074a43b4bd1f81221c2adfa0e950a3def2f42
arga.wirawa <arga.wirawan@mail.ugm.ac.id>
2020-09-05 23:47:08 +0700

    add scss

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Image404 from '../../public/page404'; 
+import Image404 from '../assets/page404.png'; 
 
 class ErrorPage extends Component {
   render() {
     return (
-        <div>
-          <img src={Image404}></img>
+        <div style={{display:"flex", justifyContent:"center", alignContent:"center", marginTop:"50px"}}>
+          <img src={Image404} alt="Error"></img>
         </div>
     );
   }
